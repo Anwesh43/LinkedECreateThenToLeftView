79292c76518cb671c5e3e515e22790bec857a9a0
Anwesh43 <anweshthecool0@gmail.com>
2021-06-11 07:06:07 +0530

    adding constants and libraries

--- ecreatethenleftview/src/main/java/com/example/ecreatethenleftview/ECreateThenLeftView.kt
+++ ecreatethenleftview/src/main/java/com/example/ecreatethenleftview/ECreateThenLeftView.kt
@@ -1,2 +1,27 @@
 package com.example.ecreatethenleftview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 5.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+val rot : Float = 180f
