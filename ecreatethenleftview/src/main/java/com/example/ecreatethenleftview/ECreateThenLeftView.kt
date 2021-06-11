6fef039658b71d35ccde871b1dba01f2f16e5e55
Anwesh43 <anweshthecool0@gmail.com>
2021-06-11 07:13:04 +0530

    adding Canvas extension methods

--- ecreatethenleftview/src/main/java/com/example/ecreatethenleftview/ECreateThenLeftView.kt
+++ ecreatethenleftview/src/main/java/com/example/ecreatethenleftview/ECreateThenLeftView.kt
@@ -30,3 +30,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawECreateThenLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 - (w / 2 + size) * sc4, h / 2)
+    rotate(-rot * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        for (k in 0..1) {
+            save()
+            rotate(deg * sc2)
+            drawLine(0f, 0f, 0f, size * 0.5f * sc1, paint)
+            restore()
+        }
+        restore()
+    }
+    drawLine(0f, 0f, size * 0.25f * sc3, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawECTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawECreateThenLeft(scale, w, h, paint)
+}
