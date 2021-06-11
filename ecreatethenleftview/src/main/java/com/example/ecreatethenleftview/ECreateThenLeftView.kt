243e2b81f48a496340ea2162d60e50814b49add1
Anwesh43 <anweshthecool0@gmail.com>
2021-06-11 07:44:53 +0530

    adding static create method

--- ecreatethenleftview/src/main/java/com/example/ecreatethenleftview/ECreateThenLeftView.kt
+++ ecreatethenleftview/src/main/java/com/example/ecreatethenleftview/ECreateThenLeftView.kt
@@ -215,4 +215,13 @@ class ECreateThenLeftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ECreateThenLeftView {
+            val view : ECreateThenLeftView = ECreateThenLeftView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
